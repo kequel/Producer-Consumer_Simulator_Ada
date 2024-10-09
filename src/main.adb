6b2e82faecb0bac26b98884bc1f4144a68a59cf6
martynkaqhe <62442310+martynkaqhe@users.noreply.github.com>
2024-10-09 22:59:33 +0200

    too long/xd

--- src/main.adb
+++ src/main.adb
@@ -98,116 +98,126 @@ Assembly_Name: constant array (Assembly_Type) of String(1 .. 30)
          Consumer_Nb := Consumer_Number;
          Consumption := Consumption_Time;
       end Start;
+      
       Put_Line(ESC & "[96m" & "C: Started consumer " & Consumer_Name(Consumer_Nb) & ESC & "[0m");
       loop
-         delay Duration(Random_Consumption.Random(G)); --  simulate consumption
+         delay Duration(Random_Consumption.Random(G)); -- simulate consumption
          Assembly_Type := Random_Assembly.Random(GA);
-         select --spotkanie selektywne z przeterminowaniem
-         B.Deliver(Assembly_Type, Assembly_Number);
-         Put_Line(ESC & "[96m" & "C: " & Consumer_Name(Consumer_Nb) & " takes assembly " &
-                    Assembly_Name(Assembly_Type) & " number " &
-                    Integer'Image(Assembly_Number) & ESC & "[0m");
-         or --spotkanie selektywne z przeterminowaniem
-            delay 1.0;
+         select
+            -- Attempt to deliver an assembly
+            B.Deliver(Assembly_Type, Assembly_Number);
+            Put_Line("XD");
+         or
+              -- Wait for too long if the delivery is not ready
+            delay 2.0;
             Put_Line("Consumer waited too long. Does not want it anymore.");
-            --musi jakos chyba usuwac ta prosbe - nie dzala
-         end select; --spotkanie selektywne z przeterminowaniem
+         end select;
       end loop;
    end Consumer;
 
-   task body Buffer is
-      Storage_Capacity: constant Integer := 30;
-      type Storage_type is array (Producer_Type) of Integer;
-      Storage: Storage_type
-        := (0, 0, 0, 0, 0);
-      Assembly_Content: array(Assembly_Type, Producer_Type) of Integer
-        := ((2, 1, 2, 0, 2),
-            (1, 2, 0, 1, 0),
-            (3, 2, 2, 0, 1));
-      Max_Assembly_Content: array(Producer_Type) of Integer;
-      Assembly_Number: array(Assembly_Type) of Integer
-        := (1, 1, 1);
-      In_Storage: Integer := 0;
+  task body Buffer is
+   Storage_Capacity: constant Integer := 30;
+   type Storage_type is array (Producer_Type) of Integer;
+   Storage: Storage_type
+     := (0, 0, 0, 0, 0);
+   Assembly_Content: array(Assembly_Type, Producer_Type) of Integer
+     := ((2, 1, 2, 0, 2),
+         (1, 2, 0, 1, 0),
+         (3, 2, 2, 0, 1));
+   Max_Assembly_Content: array(Producer_Type) of Integer;
+   Assembly_Number: array(Assembly_Type) of Integer
+     := (1, 1, 1);
+   In_Storage: Integer := 0;
 
-      procedure Setup_Variables is
-      begin
-         for W in Producer_Type loop
-            Max_Assembly_Content(W) := 0;
-            for Z in Assembly_Type loop
-               if Assembly_Content(Z, W) > Max_Assembly_Content(W) then
-                  Max_Assembly_Content(W) := Assembly_Content(Z, W);
-               end if;
-            end loop;
-         end loop;
-      end Setup_Variables;
+   -- Random delay for delivery
+   subtype Delivery_Delay_Range is Integer range 1 .. 3;
+   package Random_Delivery is new Ada.Numerics.Discrete_Random(Delivery_Delay_Range);
+   G_Delivery: Random_Delivery.Generator;
 
-      function Can_Accept(Product: Producer_Type) return Boolean is
-      begin
-         if In_Storage >= Storage_Capacity then
-            return False;
-         else
-            return True;
-         end if;
-      end Can_Accept;
-
-      function Can_Deliver(Assembly: Assembly_Type) return Boolean is
-      begin
-         for W in Producer_Type loop
-            if Storage(W) < Assembly_Content(Assembly, W) then
-               return False;
-            end if;
-         end loop;
-         return True;
-      end Can_Deliver;
-
-      procedure Storage_Contents is
-      begin
-         for W in Producer_Type loop
-            Put_Line("|   Storage contents: " & Integer'Image(Storage(W)) & " "
-                     & Product_Name(W));
-         end loop;
-         Put_Line("|   Number of products in storage: " & Integer'Image(In_Storage));
-
-      end Storage_Contents;
-
+   procedure Setup_Variables is
    begin
-      Put_Line(ESC & "[91m" & "B: Buffer started" & ESC & "[0m");
-      Setup_Variables;
-      loop
-         select --spotaknie selektywne
-         accept Take(Product: in Producer_Type; Number: in Integer) do
-            if Can_Accept(Product) then
-               Put_Line(ESC & "[91m" & "B: Accepted product " & Product_Name(Product) & " number " &
-                          Integer'Image(Number)& ESC & "[0m");
-               Storage(Product) := Storage(Product) + 1;
-               In_Storage := In_Storage + 1;
-            else
-               Put_Line(ESC & "[91m" & "B: Rejected product " & Product_Name(Product) & " number " &
-                          Integer'Image(Number)& ESC & "[0m");
+      for W in Producer_Type loop
+         Max_Assembly_Content(W) := 0;
+         for Z in Assembly_Type loop
+            if Assembly_Content(Z, W) > Max_Assembly_Content(W) then
+               Max_Assembly_Content(W) := Assembly_Content(Z, W);
             end if;
-         end Take;
-            Storage_Contents;
-            or --spotaknie selektywne
-         accept Deliver(Assembly: in Assembly_Type; Number: out Integer) do
-            if Can_Deliver(Assembly) then
-               Put_Line(ESC & "[91m" & "B: Delivered assembly " & Assembly_Name(Assembly) & " number " &
-                          Integer'Image(Assembly_Number(Assembly))& ESC & "[0m");
-               for W in Producer_Type loop
-                  Storage(W) := Storage(W) - Assembly_Content(Assembly, W);
-                  In_Storage := In_Storage - Assembly_Content(Assembly, W);
-               end loop;
-               Number := Assembly_Number(Assembly);
-               Assembly_Number(Assembly) := Assembly_Number(Assembly) + 1;
-            else
-               Put_Line(ESC & "[91m" & "B: Lacking products for assembly " & Assembly_Name(Assembly)& ESC & "[0m");
-               Number := 0;
-            end if;
-            end Deliver;
-            end select; --spotaknie selektywne
-         Storage_Contents;
-
+         end loop;
       end loop;
-   end Buffer;
+   end Setup_Variables;
+
+   function Can_Accept(Product: Producer_Type) return Boolean is
+   begin
+      if In_Storage >= Storage_Capacity then
+         return False;
+      else
+         return True;
+      end if;
+   end Can_Accept;
+
+   function Can_Deliver(Assembly: Assembly_Type) return Boolean is
+   begin
+      for W in Producer_Type loop
+         if Storage(W) < Assembly_Content(Assembly, W) then
+            return False;
+         end if;
+      end loop;
+      return True;
+   end Can_Deliver;
+
+   procedure Storage_Contents is
+   begin
+      for W in Producer_Type loop
+         Put_Line("|   Storage contents: " & Integer'Image(Storage(W)) & " "
+                  & Product_Name(W));
+      end loop;
+      Put_Line("|   Number of products in storage: " & Integer'Image(In_Storage));
+   end Storage_Contents;
+
+begin
+   Put_Line(ESC & "[91m" & "B: Buffer started" & ESC & "[0m");
+   Setup_Variables;
+   loop
+      select --spotaknie selektywne
+      accept Take(Product: in Producer_Type; Number: in Integer) do
+         if Can_Accept(Product) then
+            Put_Line(ESC & "[91m" & "B: Accepted product " & Product_Name(Product) & " number " &
+                       Integer'Image(Number) & ESC & "[0m");
+            Storage(Product) := Storage(Product) + 1;
+            In_Storage := In_Storage + 1;
+         else
+            Put_Line(ESC & "[91m" & "B: Rejected product " & Product_Name(Product) & " number " &
+                       Integer'Image(Number) & ESC & "[0m");
+         end if;
+      end Take;
+      Storage_Contents;
+
+      or --spotaknie selektywne
+        
+
+      accept Deliver(Assembly: in Assembly_Type; Number: out Integer) do
+         if Can_Deliver(Assembly) then
+            Put_Line(ESC & "[91m" & "B: Delivered assembly " & Assembly_Name(Assembly) & " number " &
+                       Integer'Image(Assembly_Number(Assembly)) & ESC & "[0m");
+            for W in Producer_Type loop
+               Storage(W) := Storage(W) - Assembly_Content(Assembly, W);
+               In_Storage := In_Storage - Assembly_Content(Assembly, W);
+            end loop;
+            Number := Assembly_Number(Assembly);
+            Assembly_Number(Assembly) := Assembly_Number(Assembly) + 1;
+             delay Duration(Random_Delivery.Random(G_Delivery));
+            
+
+         else
+            Put_Line(ESC & "[91m" & "B: Lacking products for assembly " & Assembly_Name(Assembly) & ESC & "[0m");
+            Number := 0; 
+         end if;
+      end Deliver;
+
+      end select; --spotaknie selektywne
+      Storage_Contents;
+   end loop;
+end Buffer;
 
 begin
    for I in 1 .. Number_Of_Producers loop
